18ae1e9a51d0cce5d37dad95e487976ec35bdf4f
jordan-oc <73565135+jordan-oc@users.noreply.github.com>
2020-12-03 15:37:26 -0800

    Update imdbURLtest.py

--- imdbURLtest.py
+++ imdbURLtest.py
@@ -8,9 +8,12 @@ class MyTest(unittest.TestCase):
         x = giveURL("300")
         y = giveURL("Scooby Doo")
         z = giveURL("titanic")
+        a = giveURL("Before Night Falls")
+        
         
         self.assertEqual(x, "https://www.imdb.com/title/tt0416449")
         self.assertEqual(y, "https://www.imdb.com/title/tt0267913")
         self.assertEqual(z, "https://www.imdb.com/title/tt0120338")
+        self.assertEqual(a, "https://www.imdb.com/title/tt0247196")
         
 unittest.main() 
